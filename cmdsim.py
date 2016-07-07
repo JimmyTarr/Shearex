77b15a997f1e31ff8c6ad74f626383138ba8019e
MichaelTarr <Michael.tarr@port.ac.uk>
2016-07-07 13:10:48 +0100

    Edit to variable names for readability

--- cmdsim.py
+++ cmdsim.py
@@ -3,18 +3,18 @@
 import shearex, sys
 import numpy as np
 
-a,b,c=np.float(sys.argv[1]),np.float(sys.argv[2]),np.float(sys.argv[3])
-e=int(sys.argv[5])
+skysz,eint,pix=np.float(sys.argv[1]),np.float(sys.argv[2]),np.float(sys.argv[3])
+uvfrac=int(sys.argv[5])
 
 if str.isdigit(sys.argv[4]):
-    d=int(sys.argv[4])
+    kappa=int(sys.argv[4])
 elif str.isdigit(str.split(str.split(sys.argv[4],',')[0],'.')[0]):
-    d=eval('[' + sys.argv[4] + ']')
+    kappa=eval('[' + sys.argv[4] + ']')
 else:
-    d=sys.argv[4]
+    kappa=sys.argv[4]
 
-print a,b,c,d
+print skysz,eint,pix,kappa
 
-shearex.simsky(a,b,c,d,e)
+shearex.simsky(skysz,eint,pix,kappa,uvfrac)
 
 exit()
